3d9bfc54cd9faf93b0a567eccb236bd7feb5721d
Will Jackson <wjackson2112@gmail.com>
2016-10-10 21:51:37 -0600

    Simplify Basic Entity

--- Entity/include/Entity.hpp
+++ Entity/include/Entity.hpp
@@ -1,31 +1,10 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
-#include <SDL2/SDL.h>
-#include <iostream>
-#include "EventManager.h"
-#include "Globals.h"
-#include "AssetFactory.h"
-#include "Rect.h"
-
 class Entity
 {
-protected:
-	Rect rect;
-	SDL_Texture* texture;
-public:
-
-	Entity(string texture_file, AssetFactory* assetFactory);
-	
-	virtual char* type();
-
-	virtual void render(SDL_Renderer*);
-	virtual void update(int frameTime){};
-
-	SDL_Point getCenter();
-
-	virtual void setRect(Rect rect);
-	Rect getRect();
+	virtual void render() = 0;
+	virtual void update(int frameTime) = 0;
 };
 
 #endif
